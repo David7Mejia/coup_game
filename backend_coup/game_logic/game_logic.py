8cb78dd32343ca7163d6361954a41abb4f8fd404
David Mejia <david7mejia@gmail.com>
2023-12-01 20:13:36 -0500

    initialize_game function

--- backend_coup/game_logic/game_logic.py
+++ backend_coup/game_logic/game_logic.py
@@ -1,9 +1,14 @@
+import random
+
 def initialize_game(num_players):
-    if num_players < 3:
-        raise ValueError("At least 3 players are required to start the game.")
+    if num_players < 2:
+        raise ValueError("At least 2 players are required to start the game.")
 
     if num_players > 6:
         raise ValueError("The maximum number of players allowed is 6.")
+
+    # Define the character card types and quantities
+    initial_card_counts = {'Duke': 3, 'Assassin': 3, 'Captain': 3, 'Ambassador': 3, 'Contessa': 3}
 
     # Create player objects
     players = []
@@ -12,7 +17,7 @@ def initialize_game(num_players):
             'id': player_id,
             'name': f'Player {player_id + 1}',  # Assign default names
             'coins': 2,  # Give each player 2 coins at the beginning
-            # Add any other player attributes as needed
+            'cards': [],  # Initialize empty hand for each player
         }
         players.append(player)
 
@@ -24,9 +29,25 @@ def initialize_game(num_players):
         'players': players,
         'current_turn': 0,  # Initialize the turn to the first player
         'game_status': 'not_started',
-        'total_coins_left': total_coins,  # Track the total coins available
-        # Add other game-specific attributes as needed
+        'total_coins_in_pile': total_coins,  # Track the total coins available
+        'deck_card_counts': initial_card_counts.copy(),  # Track card counts
     }
+
+    # Create a deck of character cards based on initial quantities
+    deck = []
+    for card, count in initial_card_counts.items():
+        deck.extend([card] * count)
+
+    # Shuffle the deck to randomize card distribution
+    random.shuffle(deck)
+
+    # Deal 2 cards to each player, keeping the distribution hidden
+    for player in players:
+        for _ in range(2):
+            card = deck.pop()  # Take a card from the top of the shuffled deck
+            player['cards'].append(card)  # Add the card to the player's hand
+            # Decrement the card count in game state
+            game_state['deck_card_counts'][card] -= 1
 
     # Return the initialized game state
     return game_state
