a924e5cd1bcdcc425b4067c902b2f670418f3043
David Mejia <david7mejia@gmail.com>
2023-12-01 20:01:12 -0500

    add player coin functionality

--- backend_coup/game_logic/game_logic.py
+++ backend_coup/game_logic/game_logic.py
@@ -1,6 +1,6 @@
 def initialize_game(num_players):
-    if num_players < 2:
-        raise ValueError("At least 2 players are required to start the game.")
+    if num_players < 3:
+        raise ValueError("At least 3 players are required to start the game.")
 
     if num_players > 6:
         raise ValueError("The maximum number of players allowed is 6.")
@@ -11,15 +11,20 @@ def initialize_game(num_players):
         player = {
             'id': player_id,
             'name': f'Player {player_id + 1}',  # Assign default names
+            'coins': 2,  # Give each player 2 coins at the beginning
             # Add any other player attributes as needed
         }
         players.append(player)
+
+    # Calculate the total number of coins available in the pile
+    total_coins = 50 - (num_players * 2)  # 2 coins per player
 
     # Initialize the game state
     game_state = {
         'players': players,
         'current_turn': 0,  # Initialize the turn to the first player
         'game_status': 'not_started',
+        'total_coins_left': total_coins,  # Track the total coins available
         # Add other game-specific attributes as needed
     }
 
